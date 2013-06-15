07cba39e10df0975bfe16c2482b52b2b9ba088d0
Tum Chaturapruek <tummykung@gmail.com>
2013-06-15 14:50:34 -0700

    change name

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tryloop::Application.config.secret_token = '54bf2df41a50099998bd4951392e3be1229a408df7838c7c200d3ac6667b65de1b34a3c43bc86bde57921fda96e1ff011a1d07e5fd5008c26297861841ae83ce'
