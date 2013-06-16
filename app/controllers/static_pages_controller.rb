23f4b32f9c71d2f926ea71c7a3c62f4be0b5e50a
Tum Chaturapruek <tummykung@gmail.com>
2013-06-16 11:29:28 -0700

    some final touch

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,6 +6,20 @@ class StaticPagesController < ApplicationController
     end
   end
 
+  def explore
+  	 if signed_in?
+      @micropost  = current_user.microposts.build
+      @feed_items = current_user.feed.paginate(page: params[:page])
+    end
+  end
+
+  def reflect
+  	 if signed_in?
+      @micropost  = current_user.microposts.build
+      @feed_items = current_user.feed.paginate(page: params[:page])
+    end
+  end
+
   def help
   end
 end
