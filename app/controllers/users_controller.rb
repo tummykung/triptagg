63c8c6a2f5b096c9d90dbf21ccdc81f1ba74f584
Tum Chaturapruek <tummykung@gmail.com>
2013-06-15 19:19:51 -0700

    add microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
 
 	def show
 		@user = User.find(params[:id])
-		@microposts = @user.microposts.paginate(page: params[:page])
+		#TODO@microposts = @user.microposts.paginate(page: params[:page])
 	end
 
 	def index
