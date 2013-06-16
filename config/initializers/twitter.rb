d9b84ec56356f6097f849fe4642dd8c31484d930
Tum Chaturapruek <tummykung@gmail.com>
2013-06-16 00:05:18 -0700

    add twitter aggregration

---
+++ config/initializers/twitter.rb
@@ -0,0 +1,6 @@
+Twitter.configure do |config|
+  config.consumer_key = 'XCxqS5hWZL1KGHCRJY2nQ'
+  config.consumer_secret = 'E45uAqQSBD4DkTE4o7OxQBsYZmENFGrznTgg0Jgok'
+  config.oauth_token = '1521170389-laWPCloWfxCmru7UDtPtdWtacVoNh5nn3yjqRii'
+  config.oauth_token_secret = 'rqQZ7Ni04VbwXqvMJ7oeZMxNyuljbETjHhrYWgTAqg0'
+end
--- config/routes.rb
+++ config/routes.rb
@@ -17,14 +17,14 @@ Tryloop::Application.routes.draw do
 
   root                to: 'static_pages#home'
   match '/help',      to: 'static_pages#help'
-  match '/prepare',  to: 'static_pages#prepare'
-  match '/explore',  to: 'static_pages#explore'
-  match '/reflect',  to: 'static_pages#reflect'
+  match '/prepare',   to: 'static_pages#prepare'
+  match '/explore',   to: 'static_pages#explore'
+  match '/reflect',   to: 'static_pages#reflect'
   match '/about',     to: 'static_pages#about'
   match '/contact',   to: 'static_pages#contact'
   match '/signup',    to: 'users#new'
-  match '/signin',  to: 'sessions#new' 
-  match '/signout',  to: 'sessions#destroy'
+  match '/signin',    to: 'sessions#new' 
+  match '/signout',   to: 'sessions#destroy'
 
 
   # The priority is based upon order of creation:
